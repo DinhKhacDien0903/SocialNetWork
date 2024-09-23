1f77b1868fe695b819c51a4265b04fbb56926e58
viettt76 <viettt76@gmail.com>
2024-09-23 13:15:36 +0700

    Login & Sign up & Default layout

--- Project/SocialNetworkFrontend/src/App.js
+++ Project/SocialNetworkFrontend/src/App.js
@@ -1,17 +1,84 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import routes from './routes';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { BrowserRouter, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import routes from '~/routes';
+import { SetupInterceptors } from '~/utils/axios';
+import DefaultLayout from '~/layouts/DefaultLayout';
+
+function NavigateFunctionComponent() {
+    let navigate = useNavigate();
+    const [ran, setRan] = useState(false);
+
+    if (!ran) {
+        SetupInterceptors(navigate);
+        setRan(true);
+    }
+    return <></>;
+}
 
 function App() {
     return (
         <BrowserRouter>
+            <NavigateFunctionComponent />
+            {/* <FetchUserInfo /> */}
             <Routes>
                 {routes.map((route, index) => {
                     const Page = route.component;
-                    return <Route key={`route-${index}`} path={route.path} element={<Page />} />;
+                    const Layout = DefaultLayout;
+                    if (route.layout) {
+                        Layout = route.layout;
+                    } else if (route.layout === null) {
+                        Layout = React.Fragment;
+                    }
+                    return (
+                        <Route
+                            key={`route-${index}`}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
                 })}
             </Routes>
         </BrowserRouter>
     );
+}
+
+function FetchUserInfo() {
+    const dispatch = useDispatch();
+    const location = useLocation();
+
+    useEffect(() => {
+        const fetchPersonalInfo = async () => {
+            try {
+                const res = await getPersonalInfoService();
+                dispatch(
+                    actions.saveUserInfo({
+                        id: res?.id,
+                        firstName: res?.firstName,
+                        lastName: res?.lastName,
+                        age: res?.age,
+                        avatar: res?.avatar,
+                        homeTown: res?.homeTown,
+                        school: res?.school,
+                        workplace: res?.workplace,
+                    }),
+                );
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        if (location.pathname !== '/login') {
+            fetchPersonalInfo();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return null;
 }
 
 export default App;
--- Project/SocialNetworkFrontend/src/routes.js
+++ Project/SocialNetworkFrontend/src/routes.js
@@ -1,9 +1,11 @@
 import Home from '~/pages/Home';
 import Search from '~/pages/Search';
+import Login from '~/pages/Login';
 
 const routes = [
     { path: '/', component: Home },
     { path: '/search', component: Search },
+    { path: '/login', component: Login },
 ];
 
 export default routes;
