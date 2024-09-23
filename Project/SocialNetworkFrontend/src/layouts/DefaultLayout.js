c5134150709e6ac3bd1efc22ab170db07737d9a9
viettt76 <viettt76@gmail.com>
2024-09-23 14:20:49 +0700

    Post

--- Project/SocialNetworkFrontend/src/layouts/DefaultLayout.js
+++ Project/SocialNetworkFrontend/src/layouts/DefaultLayout.js
@@ -4,7 +4,7 @@ const DefaultLayout = ({ children }) => {
     return (
         <div className="d-flex">
             <Sidebar />
-            <div style={{ marginLeft: '7.6rem' }}>{children}</div>
+            <div style={{ marginLeft: '7.6rem', flex: 1 }}>{children}</div>
         </div>
     );
 };
--- Project/SocialNetworkFrontend/src/pages/Home/Home.js
+++ Project/SocialNetworkFrontend/src/pages/Home/Home.js
@@ -1,8 +1,10 @@
+import Post from '~/components/Post';
+
 const Home = () => {
     return (
-        <>
-            <div>Home</div>
-        </>
+        <div className="d-flex justify-content-center mt-5">
+            <Post postInfo={{ id: 1 }} />
+        </div>
     );
 };
 
